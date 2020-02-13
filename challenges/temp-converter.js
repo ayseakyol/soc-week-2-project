4a7b0cd89358a84325c4b7afcf4d4aae673b94d1
ayseakyol <ayseakyol.22309@gmail.com>
2020-02-13 22:53:51 +0100

    temp-converter

--- challenges/temp-converter.js
+++ challenges/temp-converter.js
@@ -2,17 +2,26 @@ console.log('tempConverter script has loaded');
 
 // write this function!
 function tempConverter(temperature, degrees) {
+  if (degrees === 'celcius') {
+  result = (temperature - 32)*5/9;
+} else if (degrees === 'farenheit') {
+   result = 9*(temperature/5)+32;
+} 
+else {
+ return degrees + ' is not supported';
+}
+return result;
 }
 
 // if the user wants to convert to farenheit
-console.assert(tempConverter(53, 'farenheit') === 127.4, 'first');
+console.assert(tempConverter(53, 'farenheit') === 127.40, 'first');
 console.assert(tempConverter(0, 'farenheit') === 32, 'second');
 console.assert(tempConverter(-40, 'farenheit') === -40, 'third');
 
 // if the user wants to convert to celcius
-console.assert(tempConverter(0, 'celcius') === 32, 'fourth');
-console.assert(tempConverter(4, 'celcius') === 39.2, 'fifth');
-console.assert(tempConverter(12.4, 'celcius') === 54.32, 'sixth');
+console.assert(tempConverter(0, 'celcius') === -17.78, 'fourth');
+console.assert(tempConverter(4, 'celcius') === -15.56, 'fifth');
+console.assert(tempConverter(12.4, 'celcius') === -10.89, 'sixth');
 
 // if the user inputs an invalid degree
 console.assert(tempConverter(34, 'toad') === 'toad is not supported', 'seventh');
@@ -23,6 +32,7 @@ console.assert(tempConverter(500, 'Farenheit') === 'Farenheit is not supported',
 function tempConverterHandler() {
   // prompt the user for a noun, verb and adjective
   const userTempStr = prompt('enter a temperature to convert');
+  const userTemp = Number(userTempStr);
   // cast userDegreesStr to a Number, and assign the value to userDecrees
   const userDegrees = prompt('would you like to convert to farenheit or celcius?');
 
@@ -30,9 +40,12 @@ function tempConverterHandler() {
 
   // perform core logic
   // write this line!
+  const result = tempConverter(userTemp, userDegrees);
+
 
   // alert result for the user
   // write this line!
+  alert(result.toFixed(2));
 
   // log action for the developer
   console.log('\n--- tempConverter ---');
